5d29b6bf2a51f45d2f7d0a953711c70423c524af
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-12-01 10:00:06 -0500

    reset cards

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let colors = ["red","blue","red","yellow","yellow","green","blue","green"]
 let cards = []
+let clicks = 0
 
 function setup() {
   createP("Matching Game")
@@ -7,12 +8,26 @@ function setup() {
   for (let i=0; i<8; i++) {
     cards[i] = createButton("Card")
     //cards[i] means one button
-    cards[i].style("background-color",colors[i])
+    //cards[i].style("background-color",colors[i])
+    cards[i].id(i)
     cards[i].mouseClicked(onCardClick)
   }  
 }
 
 function onCardClick() {
-  let thisColor = this.style("background-color")
-  console.log(thisColor)
+  clicks++
+  console.log(this.id())
+  
+  this.style("background-color",colors[this.id()])
+  
+  if (clicks > 2) {
+    resetButtons()
+  }
+}
+
+function resetButtons() {
+  clicks = 0
+  for (let i=0; i < 8; i++) {
+    cards[i].style("background-color","grey")
+  }
 }
